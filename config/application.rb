3bcf8f00bfb5e2006f56977df0bb87b4c49d1fbb
root <root@localhost.localdomain>
2018-06-06 07:57:05 +0000

    initial commit

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,6 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
 
 module Testapp
   class Application < Rails::Application
@@ -23,6 +22,8 @@ module Testapp
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    
+    config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
